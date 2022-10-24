9645e722644ed4e77cb4f6a4533976713cbdbda1
pineone_seo21230 <seo21230@pineone.com>
2022-10-24 15:31:35 +0900

    add list page layout

---
+++ client/src/page/list/ListCreatePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="template-layout">
+    <div class="title-text"><div>Restaurant</div></div>
+    <div class="submit-layout">
+      <input placeholder="Title" />
+      <input placeholder="Address" />
+      <input placeholder="Number" />
+      <input placeholder="IMG Link" />
+      <input placeholder="Description" />
+      <button class="bgcolor-green" @click="handleAddList">SUBMIT</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/assets/css/layout.css";
+
+export default {
+  name: "ListCreatePage",
+  methods: {
+    handleAddList() {
+      console.log("list 등록");
+      this.$router.push("/list");
+    },
+  },
+};
+</script>
--- client/src/page/list/ListPage.vue
+++ client/src/page/list/ListPage.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="template-layout">
-    <div class="title-text">Restaurant</div>
+    <div class="title-text">
+      <div>Restaurant</div>
+      <button class="bgcolor-orange" @click="handleAddList">ADD LIST</button>
+    </div>
     <div class="list-layout">
       <div class="list-box">
         <div class="img-box">
@@ -43,5 +46,10 @@
 import "@/assets/css/layout.css";
 export default {
   name: "ListPage",
+  methods: {
+    handleAddList() {
+      this.$router.push("/create");
+    },
+  },
 };
 </script>
