8d537bc394ee2ea938b4e7ccf04443d4c1a4acb8
pineone_seo21230 <seo21230@pineone.com>
2022-10-24 14:36:04 +0900

    listPage Layout

--- client/src/page/list/ListPage.vue
+++ client/src/page/list/ListPage.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>
-    <div>리스트 페이지입니다.</div>
+  <div class="template-layout">
+    <div class="title-text">Restaurant</div>
+    <div class="list-layout">
+      <div class="list-box">
+        <div class="img-box">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
+          />
+        </div>
+        <div class="text-box">
+          <div class="list-title">레스토랑 이름영역입니다</div>
+          <div class="list-discription">레스토랑 설명영역입니다</div>
+        </div>
+      </div>
+      <div class="list-box">
+        <div class="img-box">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
+          />
+        </div>
+        <div class="text-box">
+          <div class="list-title">레스토랑 이름영역입니다</div>
+          <div class="list-discription">레스토랑 설명영역입니다</div>
+        </div>
+      </div>
+      <div class="list-box">
+        <div class="img-box">
+          <img
+            src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
+          />
+        </div>
+        <div class="text-box">
+          <div class="list-title">레스토랑 이름영역입니다</div>
+          <div class="list-discription">레스토랑 설명영역입니다</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
+import "@/assets/css/layout.css";
 export default {
   name: "ListPage",
 };
