20e0c22c19dc8118ce77e59f247f0dca322937dc
pineone_seo21230 <seo21230@pineone.com>
2022-10-25 17:01:50 +0900

    delete root div in restaurant page

--- client/src/page/list/ListPage.vue
+++ client/src/page/list/ListPage.vue
@@ -1,47 +1,41 @@
 <template>
-  <div>
-    <div v-if="!isList" class="restaurant-router-container">
-      <router-view />
-    </div>
-    <div v-if="isList" class="restaurant-list-container">
-      <div class="title-text">
-        <div>Restaurant</div>
-        <button class="bgcolor-orange" @click="handleAddList">ADD LIST</button>
+  <router-view v-if="!isList" />
+  <div v-if="isList" class="title-text">
+    <div>Restaurant</div>
+    <button class="bgcolor-orange" @click="handleAddList">ADD LIST</button>
+  </div>
+  <div class="content-layout">
+    <div class="list-box">
+      <div class="img-box">
+        <img
+          src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
+        />
       </div>
-      <div class="content-layout">
-        <div class="list-box">
-          <div class="img-box">
-            <img
-              src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
-            />
-          </div>
-          <div class="text-box">
-            <div class="list-title">레스토랑 이름영역입니다</div>
-            <div class="list-discription">레스토랑 설명영역입니다</div>
-          </div>
-        </div>
-        <div class="list-box">
-          <div class="img-box">
-            <img
-              src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
-            />
-          </div>
-          <div class="text-box">
-            <div class="list-title">레스토랑 이름영역입니다</div>
-            <div class="list-discription">레스토랑 설명영역입니다</div>
-          </div>
-        </div>
-        <div class="list-box">
-          <div class="img-box">
-            <img
-              src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
-            />
-          </div>
-          <div class="text-box">
-            <div class="list-title">레스토랑 이름영역입니다</div>
-            <div class="list-discription">레스토랑 설명영역입니다</div>
-          </div>
-        </div>
+      <div class="text-box">
+        <div class="list-title">레스토랑 이름영역입니다</div>
+        <div class="list-discription">레스토랑 설명영역입니다</div>
+      </div>
+    </div>
+    <div class="list-box">
+      <div class="img-box">
+        <img
+          src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
+        />
+      </div>
+      <div class="text-box">
+        <div class="list-title">레스토랑 이름영역입니다</div>
+        <div class="list-discription">레스토랑 설명영역입니다</div>
+      </div>
+    </div>
+    <div class="list-box">
+      <div class="img-box">
+        <img
+          src="https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032__480.jpg"
+        />
+      </div>
+      <div class="text-box">
+        <div class="list-title">레스토랑 이름영역입니다</div>
+        <div class="list-discription">레스토랑 설명영역입니다</div>
       </div>
     </div>
   </div>
