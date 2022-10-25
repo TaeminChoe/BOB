0afcaf1c4c15967b7023bb8c46827ad85beb9655
TaeWook <didxodnr0769@gmail.com>
2022-10-25 15:39:21 +0900

    슬라이드 메뉴 1. 메뉴 오픈시 배경 클릭 방지 요소 추가

--- client/src/components/template/Header.vue
+++ client/src/components/template/Header.vue
@@ -12,12 +12,6 @@
       />
     </div>
   </div>
-  <!-- <header class="header-container">
-    <router-link to="/home" class="logo-container">
-      <img src="https://www.pineone.com/images/logo.png" />
-    </router-link>
-    <div @click="handleClickMenu">햄버거로고</div>
-  </header> -->
 </template>
 
 <script>
@@ -30,24 +24,3 @@ export default {
   },
 };
 </script>
-
-<style>
-/* .header-container {
-  width: 100%;
-  height: 75px;
-  background-color: #415c82;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: white;
-  font-size: 22px;
-}
-
-.header-container .logo-container {
-  margin-left: 20px;
-}
-
-.hamburger-container {
-  font-size: 12px;
-} */
-</style>
