0720543950aeaa72d54706e15398b9b7ef3bcccc
TaeWook <didxodnr0769@gmail.com>
2022-10-26 10:26:43 +0900

    API 리스트 작성

--- client/src/gql/service-api.js
+++ client/src/gql/service-api.js
@@ -9,7 +9,7 @@ const Apollo = new ApolloClient({
 
 /** 회원가입 요청 */
 const createUser = (param) => {
-  Apollo.mutate({
+  return Apollo.mutate({
     mutation: SIGN_UP,
     variables: {
       input: param,
@@ -19,10 +19,49 @@ const createUser = (param) => {
 
 /** 로그인 요청 */
 const getAccountInfo = (param) => {
-  Apollo.query({
+  return Apollo.query({
     query: LOGIN,
     variables: param,
   });
 };
 
-export { createUser, getAccountInfo };
+/** 업소 조회 요청 */
+const getStore = (param) => {
+  console.log(param);
+};
+
+/** 업소 생성 요청 */
+const createStore = (param) => {
+  console.log(param);
+};
+
+/** 업소 수정 요청 */
+const updateStore = (param) => {
+  console.log(param);
+};
+
+/** 공지사항 조회 요청 */
+const getNotice = (param) => {
+  console.log(param);
+};
+
+/** 공지사항 생성 요청 */
+const createNotice = (param) => {
+  console.log(param);
+};
+
+/** 공지사항 삭제 요청 */
+const deleteNotice = (param) => {
+  console.log(param);
+};
+
+export {
+  getStore,
+  createStore,
+  updateStore,
+  getAccountInfo,
+  createUser,
+  getNotice,
+  createNotice,
+  deleteNotice,
+};
